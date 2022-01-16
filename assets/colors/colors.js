a3066b00dc70c69ca63bee83f3cea87d2ad3b594
unknown <joaobndomingues@gmail.com>
2022-01-16 12:41:17 +0000

    minor changes

--- assets/colors/colors.js
+++ assets/colors/colors.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const cores = {
     cor_off: '#C54554',
     cor_on: '#59D6B8',
@@ -12,19 +11,4 @@ const cores = {
     cria_conta: "#A1A4B2"
 };
 
-=======
-const cores = {
-    cor_off: '#C54554',
-    cor_on: '#59D6B8',
-    fundo_ecra: '#FFFFFF',
-    fundo_sensores: '#444359',
-    botoes: '#8E97FD',
-    dicas_fundo_1: '#E6E7F2',
-    dicas_letras_1: '#444D75',
-    dicas_fundo_2: '#FFE0D7',
-    dicas_letras_2: '#F6754D', 
-    cria_conta: "#A1A4B2"
-};
-
->>>>>>> 4480eda0772e64ef04174da684124cb2b2767383
 export default cores;
